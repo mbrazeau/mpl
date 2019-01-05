258b9308bd2ea0a9381440ac0a23528425aeaf4b
Martin Brazeau <martin.brazeau@gmail.com>
2019-01-05 20:44:25 +0000

    empty implementation file for character info

--- Morphy/src/Characters/mpl_charblock.c
+++
@@ -1,9 +0,0 @@
-//
-//  mpl_cblock.c
-//  Morphy
-//
-//  Created by Martin Brazeau on 29/12/2018.
-//  Copyright © 2018 Martin Brazeau. All rights reserved.
-//
-
-#include "mpl_charblock.h"
---
+++ Morphy/src/Characters/mpl_charinfo.c
@@ -0,0 +1,9 @@
+//
+//  mpl_charinfo.c
+//  Morphy
+//
+//  Created by Martin Brazeau on 05/01/2019.
+//  Copyright © 2019 Martin Brazeau. All rights reserved.
+//
+
+#include "mpl_charinfo.h"
