1a4f6cb936f0179f3bd8529bdac9d51bb7848d31
Martin Brazeau <martin.brazeau@gmail.com>
2019-01-06 14:56:15 +0000

    rm ** for desc indices; ++ws

--- Morphy/src/Trees/mpl_newick_rdr.h
+++ Morphy/src/Trees/mpl_newick_rdr.h
@@ -12,13 +12,14 @@
 #include "mpl_topol.h"
 
 typedef struct _nwk_rdr {
+    
     long nopenbr;
     long nclosebr;
     long ncomma;
     long nspace;
     long taxnamemax; // Maximum length of a taxon name
     char* namebuffer;
-    long** descindices; // At each rank of Newick hierarchy, store the descendant returns
+    
 } mpl_newick_rdr;
 
 int mpl_newick_rdr_init(long num_taxa, mpl_newick_rdr* rdr);
