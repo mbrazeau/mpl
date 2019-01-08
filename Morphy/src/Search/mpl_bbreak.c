8840131f3b5087be20c4ed1ac4d4bd91dec04ddf
Martin Brazeau <martin.brazeau@gmail.com>
2019-01-08 14:03:04 +0000

    sketch out branch-breaking routines, add TBR rerooting fun

--- Morphy/src/Search/mpl_bbreak.c
+++ Morphy/src/Search/mpl_bbreak.c
@@ -6,9 +6,19 @@
 //  Copyright © 2019 Martin Brazeau. All rights reserved.
 //
 
+#include <assert.h>
+
 #include "mpl_bbreak.h"
 
-//void mpl_do_bbreak(mpl_tree* t, mpl_bbreak* bbk)
+static int mpl_bbreak_tbr_reroot(mpl_node* tgtnbr, mpl_node* base);
+
+void mpl_do_bbreak(mpl_tree* t, mpl_bbreak* bbk)
+{
+    // Set up all the global variables.
+    // Iterate over the tree list.
+}
+
+//void mpl_branch_swap(mpl_tree* t, mpl_bbreak* bbk)
 //{
 //    long ntax = 0; // The number of taxa in the tree
 //    long nnodes = 0; // The number of nodes in the tree
@@ -43,3 +53,74 @@
 //        // Restore the tree to exactly as it was before
 //    }
 //}
+
+static int mpl_bbreak_tbr_reroot(mpl_node* tgtnbr, mpl_node* base)
+{
+    if (tgtnbr == base) {
+        return 1;
+    }
+    
+    mpl_node *n     = NULL;
+    mpl_node *p     = NULL;
+    mpl_node *q     = NULL;
+    mpl_node *r     = NULL;
+    mpl_node** rp   = NULL;
+    
+    mpl_node* n1 = tgtnbr;
+    mpl_node* n2 = tgtnbr->anc;
+    
+    n = n1->anc;
+    p = n1;
+    
+    while (n) {
+        // TODO: This can be simplified without pointers to pointers by using
+        // an intermediate variable. But for now this should work.
+        q = n->anc;
+        if (n->left == p) {
+            r = n->left;
+            rp = &n->left;
+        }
+        else {
+            r = n->right;
+            rp = &n->right;
+        }
+        *rp = n->anc;
+        n->anc = r;
+        p = n;
+        n = q;
+    }
+    
+    r   = NULL;
+    rp  = NULL;
+    
+    if (base->left == NULL) {
+        r = n->right;
+    }
+    else {
+        r = n->left;
+    }
+#ifdef DEBUG
+    assert(r);
+#endif
+    
+    if (base->anc->right == base) {
+        rp = &base->anc->right;
+    }
+    else {
+        rp = &base->anc->right;
+#ifdef DEBUG
+        assert((*rp)->left == base);
+#endif
+    }
+    
+    r->anc = base->anc;
+    *rp = r;
+    
+    base->left = n1;
+    n1->anc = base;
+    
+    base->right = n2;
+    n2->anc = base;
+    
+    return 0;
+}
--- Morphy/src/Search/mpl_bbreak.h
+++ Morphy/src/Search/mpl_bbreak.h
@@ -9,15 +9,22 @@
 #ifndef mpl_bbreak_h
 #define mpl_bbreak_h
 
+#include "mpl_search.h"
 #include "../Trees/mpl_node.h"
 #include "../Trees/mpl_tree.h"
 
 typedef struct _bbreak {
     
+    mpl_bbreak_t    bbktype;
+    long            num_nodes;
     mpl_node**      srcs;
     mpl_node**      tgts;
     unsigned long   num_rearrangs;
 
 } mpl_bbreak;
+
+mpl_bbreak* mpl_bbreak_new(long num_nodes);
+void        mpl_bbreak_delete(mpl_bbreak** bbk);
+void        mpl_do_bbreak(mpl_tree* t, mpl_bbreak* bbk);
 
 #endif /* mpl_bbreak_h */
