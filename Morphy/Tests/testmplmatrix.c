47adaf08d28fa71fe02d43f426d0f3bf7e462f21
Martin Brazeau <martin.brazeau@gmail.com>
2019-01-25 21:10:38 +0000

    tests for basic matrix reading and conversion

--- Morphy/Tests/testmplmatrix.c
+++ Morphy/Tests/testmplmatrix.c
@@ -184,3 +184,179 @@ int test_get_char_pointer_from_matrix (void)
     
     return failn;
 }
+
+int test_counting_gaps_in_matrix_columns (void)
+{
+    theader("Test counting gaps in each matrix column");
+    
+    int failn = 0;
+    
+    long ncols = 15;
+    long nrows = 7;
+    
+    char* matrix = "10000 00000 00010\
+                    0-000 1-222 00110\
+                    0-000 1-222 00110\
+                    00000 1-222 0-110\
+                    00000 1-022 00110\
+                    0-000 1-022 00110\
+                    00001 00000 0000-;";
+    
+    mpl_matrix* m = NULL;
+    m = mpl_matrix_new();
+    
+    mpl_matrix_set_ncols(ncols, m);
+    mpl_matrix_set_nrows(nrows, m);
+    mpl_matrix_attach_rawdata(matrix, m);
+    
+    long res = 0;
+    
+    res = mpl_test_matrix_count_gaps_in_column(0, m);
+    if (res != 0) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    res = 0;
+    res = mpl_test_matrix_count_gaps_in_column(1, m);
+    if (res != 3) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    res = 0;
+    res = mpl_test_matrix_count_gaps_in_column(6, m);
+    if (res != 5) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    res = 0;
+    res = mpl_test_matrix_count_gaps_in_column(11, m);
+    if (res != 1) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    res = 0;
+    res = mpl_test_matrix_count_gaps_in_column(14, m);
+    if (res != 1) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    res = 0;
+    
+//    mpl_matrix_delete(&m);
+    
+    return failn;
+}
+
+int test_parsimony_data_setup (void)
+{
+    theader("Test setting up the parsimony types and ranges");
+    
+    /*
+     This is a small test that simply checks whether the program correctly
+     partitions the data into two incipient blocks: a parset for standard
+     Fitch characters and a second block for Fitch characters with a
+     non-trivial set of inapplicable entries.
+     */
+    
+    int failn = 0;
+    
+    const long ncols = 15;
+    const long nrows = 7;
+    
+    char* matrix =
+    //                11111
+//   01234      56789 01234
+    "10000      00000 00010\
+     0-000      1-222 00110\
+     0-000      1-222 00110\
+     (12)0000   1-222 0-110\
+     00000      1-022 00110\
+     0-000      1-022 00110\
+     00001      00000 0000-;";
+    
+    mpl_matrix* m = NULL;
+    m = mpl_matrix_new();
+    
+    mpl_matrix_set_ncols(ncols, m);
+    mpl_matrix_set_nrows(nrows, m);
+    mpl_matrix_attach_rawdata(matrix, m);
+//    char* symbols = "0123456789";
+//    m->symbols = symbols;
+    
+    mpl_matrix_apply_data(m);
+    
+    mpl_test_matrix_setup_parsimony(m);
+    
+    if (m->parsets[0].start != 0 || m->parsets[0].end != 13) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    
+    if (m->parsets[1].start != 13 || m->parsets[1].end != 15) {
+        ++failn;
+        pfail;
+    }
+    else {
+        ppass;
+    }
+    
+    int i = 0;
+    int j = 0;
+    
+    for (i = 0; i < nrows; ++i) {
+        for (j = 0; j < ncols; ++j) {
+            printf("%lu", m->cbufs[0].dnset[i][j]);
+            if (j < ncols - 1) {
+                printf(",\t");
+            }
+        }
+        printf("\n");
+    }
+    
+    unsigned long expected[nrows][ncols] =
+//   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4
+    {
+        {4,  2, 2, 2, 2, 2, 2, 2, 2, 2,          2, 4, 2,          2, 2},
+        {2,  2, 2, 2, 4, 8, 8, 8, 2, 2,          4, 4, 2,          1, 1},
+        {2,  2, 2, 2, 4, 8, 8, 8, 2, 2,          4, 4, 2,          1, 1},
+        {12, 2, 2, 2, 4, 8, 8, 8, 2, 4294967295, 4, 4, 2,          2, 1},
+        {2,  2, 2, 2, 4, 2, 8, 8, 2, 2,          4, 4, 2,          2, 1},
+        {2,  2, 2, 2, 4, 2, 8, 8, 2, 2,          4, 4, 2,          1, 1},
+        {2,  2, 2, 4, 2, 2, 2, 2, 2, 2,          2, 2, 4294967295, 2, 2}
+    };
+   
+    for (i = 0; i < nrows; ++i) {
+        for (j = 0; j < ncols; ++j) {
+            if (expected[i][j] != m->cbufs[0].dnset[i][j]) {
+                ++failn;
+                pfail;
+                printf("position i: %i, %i\n", i, j);
+            }
+            else {
+                ppass;
+            }
+        }
+    }
+    
+    mpl_matrix_delete(&m);
+    
+    return failn;
+}
--- Morphy/Tests/testmplmatrix.h
+++ Morphy/Tests/testmplmatrix.h
@@ -15,9 +15,13 @@ int test_basic_matrix (void);
 int test_skip_closure (void);
 int test_skip_whitespace (void);
 int test_get_char_pointer_from_matrix (void);
+int test_counting_gaps_in_matrix_columns (void);
+int test_parsimony_data_setup (void);
 
 
 char* mpl_test_matrix_get_rawdat_ptr (const long row, const long col, mpl_matrix* m);
+long mpl_test_matrix_count_gaps_in_column(const long col, const mpl_matrix* m);
+void mpl_test_matrix_setup_parsimony(mpl_matrix* m);
 char* mpl_test_skip_closure (const char *closure, const char openc, const char closec);
 char* mpl_test_skip_whitespace (char* c);
 
