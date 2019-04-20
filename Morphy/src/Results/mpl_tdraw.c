8e9f1ad11b7b9ff82601a243fbb9c0606db3fdb2
Martin Brazeau <martin.brazeau@gmail.com>
2019-04-20 20:51:09 +0100

    +module for drawing a tree as ASCII chars

---
+++ Morphy/src/Results/mpl_tdraw.c
@@ -0,0 +1,12 @@
+//
+//  mpl_tdraw.c
+//  Morphy
+//
+//  Created by Martin Brazeau on 20/04/2019.
+//  Copyright © 2019 Martin Brazeau. All rights reserved.
+//
+
+#include "mpl_tdraw.h"
+
+static void mpl_tdraw_calc_dimensions(mpl_tree* t, mpl_tdraw* td);
+
---
+++ Morphy/src/Results/mpl_tdraw.h
@@ -0,0 +1,27 @@
+//
+//  mpl_tdraw.h
+//  Morphy
+//
+//  Created by Martin Brazeau on 20/04/2019.
+//  Copyright © 2019 Martin Brazeau. All rights reserved.
+//
+
+#ifndef mpl_tdraw_h
+#define mpl_tdraw_h
+
+#include "../Trees/mpl_tree.h"
+#include "../Taxa/mpl_taxblock.h"
+
+typedef struct _tdraw {
+    
+    int     width;
+    int     height;
+    char**  canvas;
+    
+} mpl_tdraw;
+
+mpl_tdraw* mpl_trdraw_new(mpl_tree* t, mpl_taxablock* taxa);
+void mpl_tdraw_delete(mpl_tdraw** td);
+void mpl_tdraw_print(mpl_tdraw* td);
+
+#endif /* mpl_tdraw_h */
