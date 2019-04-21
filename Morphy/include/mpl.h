0b96584bfd10cd29493a67582bca6099264f7e19
Martin Brazeau <martin.brazeau@gmail.com>
2019-04-21 21:39:27 +0100

    +interface prototypes; changes to const params

--- Morphy/include/mpl.h
+++ Morphy/include/mpl.h
@@ -24,10 +24,10 @@ mpl_handle* mpl_handle_new(void);
 void        mpl_handle_delete(mpl_handle** handl);
     
 // TODO: Use error codes
-long        mpl_get_ntax(mpl_handle handl);
-int         mpl_set_ntax(long ntax, mpl_handle handl);
-int         mpl_get_nchar(mpl_handle handl);
-long        mpl_set_nchar(long nchar, mpl_handle handl);
+long        mpl_get_ntax(const mpl_handle handl);
+int         mpl_set_ntax(const long ntax, mpl_handle handl);
+int         mpl_get_nchar(const mpl_handle handl);
+long        mpl_set_nchar(const long nchar, mpl_handle handl);
 int         mpl_load_matrix(char* matrix, mpl_handle handl);
 //          Set character types
 //          Exclude character
@@ -37,15 +37,22 @@ int         mpl_load_matrix(char* matrix, mpl_handle handl);
 //          Exclude taxon
 //          Include taxon
 //          Set character weight
+int         mpl_set_char_weight(const float weight, const long index, mpl_handle handl);
 //          Get character weight
+float       mpl_get_char_weight(const long index, mpl_handle handl);
 //          Set equal weights
+int         mpl_set_equal_weights(mpl_handle handl);
 //          Set character parsimony type
 //          Get character parsimony type
 //          Apply character settings
 //          Set hold value
+int         mpl_set_hold(const int hold, mpl_handle handl);
 //          Get hold value
+int         mpl_get_hold(const mpl_handle handl);
 //          Set maxtrees
+int         mpl_set_maxtrees(const long maxtrees, mpl_handle handle);
 //          Get maxtrees
+long        mpl_set_maxtrees(const mpl_handle handle);
 //          Set autoincrease value
 //          Get autoincrease value
 //          Set addition sequence type
@@ -54,6 +61,8 @@ int         mpl_load_matrix(char* matrix, mpl_handle handl);
 //          Get branch-swapping type
 //          Get trees by stepwise addition
 //          Get trees AS+branch-swapping
+//          Set keep number (number of trees to keep per replicate)
+//          Get keep number (number of trees to keep per replicate)
 
 #ifdef __cplusplus
 } /* extern "C" */
