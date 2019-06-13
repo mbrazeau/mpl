49acf2f45e21cfd3f240bdf68ac369e4f6e2776b
Martin Brazeau <martin.brazeau@gmail.com>
2019-06-13 12:11:35 +0100

    unit test for local shortcut (does not reroot subtree)

---
+++ Morphy/Tests/testparsshortcut.h
@@ -0,0 +1,16 @@
+//
+//  testparsshortcut.h
+//  Morphy
+//
+//  Created by Martin Brazeau on 28/05/2019.
+//  Copyright © 2019 Martin Brazeau. All rights reserved.
+//
+
+#ifndef testparsshortcut_h
+#define testparsshortcut_h
+
+#include "mpltest.h"
+
+int test_parsimony_shortcut (void);
+
+#endif /* testparsshortcut_h */
