adaeaef1f68eee755c28de908b80c6870e55d701
Martin Brazeau <martin.brazeau@gmail.com>
2019-12-30 17:25:55 +0000

    comm-out large test of shortcut

--- Morphy/Tests/main.c
+++ Morphy/Tests/main.c
@@ -104,8 +104,8 @@ int main(int argc, const char * argv[]) {
     // Test shortcut parsimony
     fails += test_parsimony_shortcut();
     fails += test_parsimony_shortcut_with_rerooting();
-//    fails += test_larger_parsimony_shortcut_with_rerooting();
-    fails += test_parsimony_shortcut_multiple_tree();
+    fails += test_larger_parsimony_shortcut_with_rerooting();
+//    fails += test_parsimony_shortcut_multiple_tree();
     
 //    fails += test_find_char_by_char_mismatches();
     
