bee28888abb1a2f600dde01d44c94a68fdb3017d
Martin Brazeau <martin.brazeau@gmail.com>
2020-03-24 16:14:01 +0000

    add test file

---
+++ Tests/testbitset.h
@@ -0,0 +1,13 @@
+//
+//  testbitset.h
+//  morphy_nui_v03
+//
+//  Created by Martin Brazeau on 24/03/2020.
+//
+
+#ifndef testbitset_h
+#define testbitset_h
+
+int test_mpl_bitset_basic (void);
+
+#endif /* testbitset_h */
